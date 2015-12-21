b626d19c6c37dcddd0e541ee4490789ead5e198d
Leonardo Cardoso <leonardo.f.cardoso@gmail.com>
2015-12-21 09:10:05 -0200

    Output in the same folder of input
    Be less verbose also

--- pdf2pdfocr.sh
+++ pdf2pdfocr.sh
@@ -22,6 +22,14 @@ INPUT_FILE=$1
 # Global var to check operating system
 OS=`uname -s`
 
+# Return complete path of argument, except last level (filename or dirname)
+complete_path() {
+	pushd `dirname "$1"` > /dev/null
+	local DIR_OUT=`pwd`
+	popd > /dev/null
+	echo "$DIR_OUT"
+}
+
 # OCRUtil Function - will be called from gnu parallel, so must use it's own variables
 ocrutil2() {
 	#echo "Param 1 is file: $1"
@@ -37,7 +45,7 @@ ocrutil2() {
 	tesseract -l por $ocrutil2_page $ocrutil2_tmpdir/$file_name_witout_ext hocr >/dev/null 2>"$ocrutil2_tmpdir/tess_err_$file_name_witout_ext.log"
 	# Downloaded hocrTransform.py from ocrmypdf software
 	python3.4 "$ocr_util2_dir"/hocrtransform.py -r 300 $ocrutil2_tmpdir/$file_name_witout_ext.hocr $ocrutil2_tmpdir/$file_name_witout_ext.pdf
-	echo "Completed character recognition on $ocrutil2_page"
+	# echo "Completed character recognition on $ocrutil2_page"
 }
 # https://www.gnu.org/software/parallel/man.html#EXAMPLE:-Composed-commands
 export -f ocrutil2
@@ -65,11 +73,11 @@ translate_path_one_file() {
 tmpfile=$(mktemp)
 TMP_DIR=$(dirname $tmpfile)
 rm $tmpfile
-echo "Temp dir is $TMP_DIR"
+# echo "Temp dir is $TMP_DIR"
 
 # Where am I?
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Script dir is $DIR"
+# echo "Script dir is $DIR"
 
 # A random prefix to support multiple execution in parallel
 PREFIX=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 5`
@@ -89,7 +97,7 @@ else
 		# File extension generated
 		EXT_IMG=pbm
 		# Deskew test - TODO add command line option to user choose to deskew
-		echo "Applying deskew"
+		# echo "Applying deskew"
 		mogrify -deskew 40% $TMP_DIR/$PREFIX-*.pbm
 	else
 		echo "$FILE_TYPE is not supported in this script. Exiting"
@@ -98,7 +106,7 @@ else
 fi
 
 # Gnu Parallel (trust me, it speed up things here)
-ls "$TMP_DIR"/"$PREFIX"*."$EXT_IMG" | awk -v tmp_dir="$TMP_DIR" -v script_dir="$DIR" '{ print $1"*"tmp_dir"*"script_dir }' | sort | parallel --progress --colsep '\*' 'ocrutil2 {1} {2} {3}'
+ls "$TMP_DIR"/"$PREFIX"*."$EXT_IMG" | awk -v tmp_dir="$TMP_DIR" -v script_dir="$DIR" '{ print $1"*"tmp_dir"*"script_dir }' | sort | parallel --colsep '\*' 'ocrutil2 {1} {2} {3}'
 
 # Join PDF files into one file that contains all OCR "backgrounds"
 PARAM_1_JOIN=`translate_path_one_file $TMP_DIR`
@@ -112,22 +120,26 @@ pdftk "$PARAM_IN_PROTECT" dump_data output /dev/null dont_ask 2>/dev/null || PDF
 
 OUTPUT_NAME=$(basename "$INPUT_FILE")
 OUTPUT_NAME_NO_EXT=${OUTPUT_NAME%.*}
+OUTPUT_DIR=$(complete_path "$INPUT_FILE")
 
 if [ "$PDF_PROTECTED" = "0" ]; then
 	# Merge OCR background PDF into the main PDF document
 	PARAM_1_MERGE=`translate_path_one_file "$INPUT_FILE"`
 	PARAM_2_MERGE=`translate_path_one_file $TMP_DIR/$PREFIX-ocr.pdf`
-	PARAM_3_MERGE=`translate_path_one_file "$OUTPUT_NAME_NO_EXT"-OCR.pdf`
+	PARAM_3_MERGE=`translate_path_one_file "$OUTPUT_DIR/$OUTPUT_NAME_NO_EXT"-OCR.pdf`
 	pdftk "$PARAM_1_MERGE" multibackground "$PARAM_2_MERGE" output "$PARAM_3_MERGE"
 else
 	echo "Original file is not an unprotected PDF. I will rebuild it (in black and white) from images  (maybe a bigger file will be generated)..."
 	convert $TMP_DIR/$PREFIX*.$EXT_IMG -compress Group4 $TMP_DIR/$PREFIX-input_unprotected.pdf
 	PARAM_1_REBUILD=`translate_path_one_file $TMP_DIR/$PREFIX-input_unprotected.pdf`
 	PARAM_2_REBUILD=`translate_path_one_file $TMP_DIR/$PREFIX-ocr.pdf`
-	PARAM_3_REBUILD=`translate_path_one_file "$OUTPUT_NAME_NO_EXT"-OCR.pdf`
+	PARAM_3_REBUILD=`translate_path_one_file "$OUTPUT_DIR/$OUTPUT_NAME_NO_EXT"-OCR.pdf`
 	pdftk "$PARAM_1_REBUILD" multibackground "$PARAM_2_REBUILD" output "$PARAM_3_REBUILD"
 fi
 
 # Cleanup (comment to preserve temp files and debug)
 rm -f $TMP_DIR/$PREFIX*.hocr $TMP_DIR/$PREFIX*.$EXT_IMG $TMP_DIR/$PREFIX*.txt $TMP_DIR/$PREFIX*.pdf $TMP_DIR/tess_err*.log $TMP_DIR/$PREFIX-ocr.pdf
+#
+echo "Success!"
+exit 0
 #
